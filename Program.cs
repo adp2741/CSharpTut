3326f334dfc50453008f9731d4772dd1198c249c
Andrew Perry <adp2741@gmail.com>
2016-11-04 09:32:25 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello how many hours of sleep did you get?");
+            int hours = int.Parse(Console.ReadLine());
+
+            if (hours >= 8)
+            {
+                Console.WriteLine("You got a lot of sleep.");
+            }
+            else
+            {
+                Console.WriteLine("{0} is not enough.", hours);
+            }
+        }
+    }
+}
